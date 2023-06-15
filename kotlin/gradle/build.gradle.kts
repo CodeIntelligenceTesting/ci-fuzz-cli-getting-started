32b89f697b894eb70661ce5d9cc90b8947db51e3
mlsandner <maren.sandner@code-intelligence.com>
2023-06-15 10:01:27 +0200

    chore: updating gradle java and kotlin examples
    ... including plugin and fixing build.gradle main class for java example

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -3,23 +3,13 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    // Include cifuzz
+    id("com.code-intelligence.cifuzz") version "1.2.0"
 }
 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
-}
-
-dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation ("org.junit.jupiter", "junit-jupiter","5.9.0")
-    // Use Jazzer for fuzzing.
-    testImplementation ("com.code-intelligence", "jazzer-junit","0.13.0")
-}
-
-tasks.test {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
 }
 
 application {
